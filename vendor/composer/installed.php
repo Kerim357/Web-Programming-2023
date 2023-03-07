a27c7d39c62c2d6ce9f9a0e3705e9884a0ed4255
Kerim357 <kerim.ahmedspahic@stu.ibu.edu.ba>
2023-03-07 12:32:12 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '76a97a565bf41d2a73019ced1f3655ba655b3356',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '76a97a565bf41d2a73019ced1f3655ba655b3356',
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'pretty_version' => 'v1.3.9',
+            'version' => '1.3.9.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikecao/flight',
+            'aliases' => array(),
+            'reference' => 'bc5e03ca59accb3ef37e31b6d4a2a9c78c7bb7ce',
+            'dev_requirement' => false,
+        ),
+    ),
+);
